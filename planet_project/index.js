fe945a674f248b7b3440e4216d20c9e592e60df6
Rasheed124 <rashdevatrealglobe@gmail.com>
2025-08-15 18:06:19 +0100

    working on file system using Fs and CSV - parser

---
+++ planet_project/index.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const { parse } = require("csv-parse");
+
+const habitablePlanets = [];
+const isHabitablePlanet = (planet) => {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+};
+
+fs.createReadStream("kepler_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+      trim: true,
+    })
+  )
+  .on("data", (data) => {
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data);
+    }
+  })
+  .on("error", (err) => {
+    console.error("Error while parsing CSV:", err);
+  })
+  .on("end", () => {
+    console.log("Finished parsing CSV.");
+    console.log(habitablePlanets); 
+    console.log(`Parsed ${habitablePlanets.length} rows.`);
+  });
